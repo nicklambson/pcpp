15b7a266e20489d751e70492b033d1d9dafd0c8b
Nick Lambson <46123794+nicklambson@users.noreply.github.com>
2022-12-06 11:24:14 +0800

    Create demo_class.py

---
+++ demo_class.py
@@ -0,0 +1,16 @@
+class Demo:
+    class_var = 'shared variable'
+
+    def __init__(self, value):
+        self.instance_var = value
+
+d1 = Demo(100)
+d2 = Demo(200)
+
+d1.another_var = 'another variable in the object'
+
+print('contents of d1:', d1.__dict__)
+print('contents of d2:', d2.__dict__)
+
+print(Demo.class_var)
+print(Demo.__dict__)
